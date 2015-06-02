fdd27a6bbfeee0e62e15736dd2594101685ce8c3
Gila <gilmor89@gmail.com>
2015-06-02 21:55:19 +0300

    first client commit

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -4,19 +4,27 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
 /**
  * Created by hagai_lvi on 5/24/15.
  */
 public class A {
+	public static JFrame frame;
 	private JPanel panel;
 	private JButton button1;
+	private JButton[] ForumButtons;
 	private JTextField textField1;
+	private JPanel ForumPanel;
 
 	public A() {
 		button1.addMouseListener(new MouseAdapter() {
@@ -24,32 +32,58 @@ public class A {
 			public void mouseClicked(MouseEvent e) {
 				System.out.println("hello");
 				textField1.setText("hi");
-				HttpClient c  = HttpClientBuilder.create().build();
-				HttpGet req = new HttpGet("http://localhost:8080/forum-system/gui/single");
-				try {
-					HttpResponse response = c.execute(req);
-					System.out.println(response.toString());
-					BufferedReader rd = new BufferedReader(
-							new InputStreamReader(response.getEntity().getContent()));
+				StringBuffer result = getResponse("http://localhost:8080/forum-system/gui/facade");
 
-					StringBuffer result = new StringBuffer();
-					String line = "";
-					while ((line = rd.readLine()) != null) {
-						result.append(line);
-					}
-				} catch (IOException e1) {
-					e1.printStackTrace();
+				Object obj=JSONValue.parse(result.toString());
+				JSONObject arr = (JSONObject)obj;
+
+				int numofForums = arr.size();
+				ForumButtons = new JButton[numofForums];
+
+				ForumPanel.setLayout(new BoxLayout(ForumPanel, BoxLayout.LINE_AXIS));
+//				frame.setPreferredSize(new Dimension(640, 480));
+
+				for (int i=0;i<numofForums;i++) {
+					ForumButtons[i] = new JButton("Forum"+i);
+					ForumButtons[i].setVisible(true);
+					ForumPanel.add(ForumButtons[i]);
 				}
+
+				ForumPanel.setPreferredSize(new Dimension(400,100));// changed it to preferredSize, Thanks!
+				frame.setContentPane( ForumPanel);
+				frame.pack();
 			}
 		});
 	}
 
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("A");
+		frame = new JFrame("A");
 		frame.setContentPane(new A().panel);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
 		frame.setVisible(true);
+
+	}
+
+	public static StringBuffer getResponse(String url){
+		HttpClient c  = HttpClientBuilder.create().build();
+		HttpGet req = new HttpGet(url);
+		try {
+			HttpResponse response = c.execute(req);
+			System.out.println(response.toString());
+			BufferedReader rd = new BufferedReader(
+					new InputStreamReader(response.getEntity().getContent()));
+
+			StringBuffer result = new StringBuffer();
+			String line = "";
+			while ((line = rd.readLine()) != null) {
+				result.append(line);
+			}
+			return result;
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		return new StringBuffer();
 	}
 
 }
---
+++ src/main/java/Forum.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.event.ComponentAdapter;
+
+/**
+ * Created by Gila on 02/06/2015.
+ */
+public class Forum {
+    private JLabel ForumName;
+    private JButton button1;
+
+    public Forum() {
+        ForumName.addComponentListener(new ComponentAdapter() {
+        });
+    }
+}
